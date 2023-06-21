9855de01b08583393adbeb5ae78d079a50cd2496
ChillyTheEevee <103384393+ChillyTheEevee@users.noreply.github.com>
2023-06-21 17:29:52 -0500

    Created ShadowcraftRelics plugin!

---
+++ src/main/java/world/sc2/shadowcraftrelics/ShadowcraftRelics.java
@@ -0,0 +1,17 @@
+package world.sc2.shadowcraftrelics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ShadowcraftRelics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
