d8652329b579b07dd41a29dfd4cd6ec852b99a27
ChillyTheEevee <103384393+ChillyTheEevee@users.noreply.github.com>
2023-06-22 13:26:42 -0500

    Revised Relic structure

--- src/main/java/world/sc2/shadowcraftrelics/managers/RelicManager.java
+++ src/main/java/world/sc2/shadowcraftrelics/managers/RelicManager.java
@@ -6,21 +6,21 @@ import org.bukkit.NamespacedKey;
 import world.sc2.shadowcraftrelics.ShadowcraftRelics;
 import world.sc2.shadowcraftrelics.relics.Relic;
 
-public class RelicsManager {
+public class RelicManager {
 
-    private static RelicsManager manager;
+    private static RelicManager manager;
 
     private static final NamespacedKey relicsKey = new NamespacedKey(ShadowcraftRelics.getPlugin(), "relic_type");
     private final BiMap<Integer, Relic> relics;
 
-    private RelicsManager() {
+    private RelicManager() {
         relics = HashBiMap.create();
 
         registerRelics();
     }
 
-    public static RelicsManager getInstance() {
-        if (manager == null) manager = new RelicsManager();
+    public static RelicManager getInstance() {
+        if (manager == null) manager = new RelicManager();
         return manager;
     }
 
--- src/main/java/world/sc2/shadowcraftrelics/relics/Relic.java
+++ src/main/java/world/sc2/shadowcraftrelics/relics/Relic.java
@@ -1,18 +1,52 @@
 package world.sc2.shadowcraftrelics.relics;
 
 import org.bukkit.Material;
+import world.sc2.shadowcraftrelics.managers.RelicManager;
 
 public abstract class Relic {
 
-    public abstract String getName();
+    protected String name;
+    protected int id;
 
-    public abstract int getId();
+    public Relic(int id, String name) {
+        this.id = id;
+        this.name = name.toLowerCase();
+    }
 
+    /**
+     * @return The assigned ID of the Relic. This ID is assigned when the Relic is instantiated in {@link RelicManager}
+     */
+    public final int getId() {
+        return id;
+    }
+
+    /**
+     * @return The internal name of a Relic. By convention, all internal Relic names follow snake_case. This name is
+     * used when identifying Relics through NBT tags, and is assigned to the Relic when it is instantiated in
+     * {@link RelicManager}
+     */
+    public final String getRelicName() {
+        return name;
+    }
+
+    /**
+     * @return The display name of a Relic. This is the name that appears on the actual ItemStack.
+     */
+    public abstract String getDisplayName();
+
+    /**
+     * @return An array of Strings that represent the lines of lore that a Relic is supposed to have upon creation
+     */
+    public abstract String[] getRelicLore();
+
+    /**
+     * @return Whether the Relic is enabled within the enabledRelics.yml config.
+     */
     public abstract boolean isEnabled();
 
+    /**
+     * @return The expected {@link Material} of a Relic.
+     */
     public abstract Material getMaterial();
-
-    // Things I might need:
-
 
 }
